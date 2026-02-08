37868d8ace71e0a7329621e6a58498f8d36eac74
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-08 21:11:09 +0530

    Day 1: Java basics – program structure, variables, and JVM entry point

---
+++ src/day01_basics/HelloWorld.java
@@ -0,0 +1,19 @@
+package day01_basics;
+
+/**
+ * Day 1 - Java Basics
+ *
+ * Topics covered:
+ * - Java program structure
+ * - main method
+ * - System.out.println
+ * - JVM entry point
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Entry point of Java application
+        System.out.println("Hello Java Developer!");
+    }
+}
