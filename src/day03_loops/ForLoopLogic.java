181ffe6674ac4c3e29d34e6f9a59dec035d8e8d1
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-10 22:30:47 +0530

    Day 3: Implemented loops using for statements

---
+++ src/day03_loops/ForLoopLogic.java
@@ -0,0 +1,19 @@
+package day03_loops;
+
+/**
+ * Day 3 – Loops
+ *
+ * Topics covered:
+ * - for loop
+ * - iteration with counter
+ */
+public class ForLoopLogic {
+
+    public static void main(String[] args) {
+
+        // Print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
