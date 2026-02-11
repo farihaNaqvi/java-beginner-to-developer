b502744c9fc69cc8d297035c0c892979bfc6f94d
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-11 22:02:38 +0530

    Day 4: Introduced custom methods and method invocation

---
+++ src/day04_methods/MethodLogic.java
@@ -0,0 +1,21 @@
+package day04_methods;
+
+/**
+ * Day 4 – Methods
+ *
+ * Topics covered:
+ * - Creating methods
+ * - Method calling
+ * - Code reusability
+ */
+
+public class MethodLogic {
+
+  public static void main(String[] args) {
+    greetUser();
+  }
+
+  public static void greetUser() {
+    System.out.println("Welcome to Java Developer Journey!");
+  }
+}
