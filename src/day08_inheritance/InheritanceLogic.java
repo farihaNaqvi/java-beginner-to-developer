c66a41e8a83dffda31457c9696beb3e619b2247e
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-16 23:00:09 +0530

    Day 8: Implemented inheritance using Employee subclass

---
+++ src/day08_inheritance/InheritanceLogic.java
@@ -0,0 +1,14 @@
+package day08_inheritance;
+
+/**
+ * Demonstrates inheritance usage
+ */
+public class InheritanceLogic {
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee("Ali", 25, 80000);
+
+        emp.displayInfo();
+    }
+}
