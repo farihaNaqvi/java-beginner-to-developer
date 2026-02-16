d635106f1702d22e9395587863a88dc73d44f7c5
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-16 22:57:47 +0530

    Day 8: Created Person base class for inheritance example

---
+++ src/day08_inheritance/Person.java
@@ -0,0 +1,22 @@
+package day08_inheritance;
+
+/**
+ * Day 8 – Inheritance
+ *
+ * Base class
+ */
+public class Person {
+
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
