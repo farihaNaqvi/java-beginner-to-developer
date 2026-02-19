08d8cf8622f36f643e88f80eb751771ccbd87f19
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-19 21:37:33 +0530

    Added Day 11 – Interfaces with Car

---
+++ src/day11_interfaces/Car.java
@@ -0,0 +1,14 @@
+package day11_interfaces;
+
+public class Car implements Vehicle {
+
+    @Override
+    public void start() {
+        System.out.println("Car is starting");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car is stopping");
+    }
+}
