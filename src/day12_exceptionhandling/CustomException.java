1ca112f3b89c698b690d9d25d1355da4b0eb1d9e
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-20 22:24:37 +0530

    Create CustomException.java

---
+++ src/day12_exceptionhandling/CustomException.java
@@ -0,0 +1,9 @@
+package day12_exceptionhandling;
+
+// Custom Exception
+public class CustomException extends Exception {
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
