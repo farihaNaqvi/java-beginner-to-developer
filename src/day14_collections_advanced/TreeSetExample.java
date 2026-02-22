514e5c1af83de70910327404b04a8635d7132779
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-02-22 22:46:34 +0530

    Added Day 14 – TreeSet examples

---
+++ src/day14_collections_advanced/TreeSetExample.java
@@ -0,0 +1,17 @@
+package day14_collections_advanced;
+
+import java.util.TreeSet;
+
+public class TreeSetExample {
+
+    public static void main(String[] args) {
+
+        TreeSet<Integer> numbers = new TreeSet<>();
+
+        numbers.add(50);
+        numbers.add(10);
+        numbers.add(30);
+
+        System.out.println("Sorted numbers: " + numbers);
+    }
+}
