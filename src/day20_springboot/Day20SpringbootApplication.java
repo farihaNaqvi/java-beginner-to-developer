9de1c1a4abff5e35c4c677e863d45ffecb19ccb3
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-03-01 00:03:10 +0530

    Update Day20SpringbootApplication.java

--- src/day20_springboot/Day20SpringbootApplication.java
+++ src/day20_springboot/Day20SpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.example.day20_springboot;
+package day20_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
