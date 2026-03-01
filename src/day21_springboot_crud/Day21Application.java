845344f7c4ed527a90e64e0a5be85f8f1b6bec17
Syeda Fariha Naqvi <71366962+farihaNaqvi@users.noreply.github.com>
2026-03-01 21:31:13 +0530

    Created Day 21 Spring Boot CRUD project structure inside src

---
+++ src/day21_springboot_crud/Day21Application.java
@@ -0,0 +1,12 @@
+package day21_springboot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day21Application.class, args);
+    }
+}
